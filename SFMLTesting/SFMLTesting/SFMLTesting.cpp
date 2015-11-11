f483f451f424d2f35933e4e61768e283ca1493d2
Ar13mis <kingtut5474@gmail.com>
2015-11-11 12:02:39 -0700

    Made render of circle and started movement detection

--- SFMLTesting/SFMLTesting/SFMLTesting.cpp
+++ SFMLTesting/SFMLTesting/SFMLTesting.cpp
@@ -4,25 +4,54 @@
 #include "stdafx.h"
 #include <SFML/Graphics.hpp>
 
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
 
+	sf::CircleShape circle(50);
+
+	circle.setFillColor(sf::Color::Cyan);
+
+	// create the window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+	// run the program as long as the window is open
 	while (window.isOpen())
 	{
+		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			// "close requested" event: we close the window
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
+		//call functions
+		circle = MovingAround (circle);
+
+		// clear the window with black color
+		window.clear(sf::Color::Black);
+
+		// draw everything here...
+		window.draw(circle);
+
+		// end the current frame
 		window.display();
 	}
 
 	return 0;
+}
+
+sf::CircleShape MovingAround (sf::CircleShape shape) 
+{
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) 
+	{
+	
+		shape.move(1, 0);
+	
+	}
+
+	return shape;
 }
