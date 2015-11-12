cf3480a9d7ccb5951d140de8761ca2af2d63782d
Ar13mis <kingtut5474@gmail.com>
2015-11-11 20:51:16 -0700

    Added better circle and started more variables

--- SFMLTesting/SFMLTesting/SFMLTesting.cpp
+++ SFMLTesting/SFMLTesting/SFMLTesting.cpp
@@ -5,16 +5,30 @@
 #include <SFML/Graphics.hpp>
 
 sf::CircleShape MovingAround(sf::CircleShape shape);
+sf::CircleShape DrawLocation(sf::CircleShape shape, float windowX, float windowY);
+
+float shapeX;
+float shapeY;
 
 int main()
 {
 
-	sf::CircleShape circle(50);
+	float radius = 40;
 
-	circle.setFillColor(sf::Color::Cyan);
+	float windowX = 800;
+	float windowY = 600;
+
+	unsigned int windowXInt = windowX;
+	unsigned int windowYInt = windowY;
+
+	sf::CircleShape circle(radius);
+
+	circle.setFillColor(sf::Color(255, 255, 255, 255));
+	circle.setOutlineThickness(10);
+	circle.setOutlineColor(sf::Color(255, 0, 0));
 
 	// create the window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	sf::RenderWindow window(sf::VideoMode(windowXInt, windowYInt), "My window");
 
 	// run the program as long as the window is open
 	while (window.isOpen())
@@ -29,7 +43,7 @@ int main()
 		}
 
 		//call functions
-		circle = MovingAround (circle);
+		circle = MovingAround(circle);
 
 		// clear the window with black color
 		window.clear(sf::Color::Black);
@@ -44,30 +58,35 @@ int main()
 	return 0;
 }
 
+
 sf::CircleShape MovingAround (sf::CircleShape shape) 
 {
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) 
 	{
 	
+		//shapeX += 1;
 		shape.move(1, 0);
-	
+
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) 
 	{
 	
+		//shapeX -= 1;
 		shape.move(-1, 0);
 	
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) 
 	{
-	
+
+		//shapeY -= 1;
 		shape.move(0, -1);
 	
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) 
 	{
 	
+		//shapeY += 1;
 		shape.move(0, 1);
 	
 	}
